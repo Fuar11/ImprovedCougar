330c9a475533d2eac684cbeb7fc9ca6d3f70307a
MonsieurMeh <monsieurmeh@devnullsoftware.com>
2025-10-08 20:31:46 -0700

    feat: spawn cougar HERE, NOW

--- SpawnRegions/CustomCougarSpawnRegion.cs
+++ SpawnRegions/CustomCougarSpawnRegion.cs
@@ -14,57 +14,43 @@ namespace ImprovedCougar.SpawnRegions
 
         }
 
-        protected override bool OverrideCalculateTargetPopulation(out int customTarget)
-        {
-            if (VanillaSpawnRegion.m_WildlifeMode == WildlifeMode.Aurora)
-            {
-                customTarget = 0;
-                return false;
-            }
-            // if cougar alive, return 1
-            // if cougar dead or not ready to appear, return 0
-            customTarget = 1 - mSpawnRegion.m_NumRespawnsPending; // this will work per region, but wont take into account a cougar that lives in multiple game zones or something
-            customTarget = Math.Max(customTarget, 0);
-            return false;
-        }
+        public override void UpdateFromManager() {} // EAF does not drive this spawn region
+
 
         protected override Type OverrideSpawnType() => typeof(CustomCougar);
-        
-        /*
-        Including some code from EAF for your convenience here, so you can see more easily what the impact of different values of your above override will do in the global system. 
-        Pay special attention to the respawn counter, the game should still be sending signals to this spawn region when the cougar passes.
 
-
-        public int CalculateTargetPopulation()
+        public void SpawnCougar(Vector3 position, Quaternion rotation, Action<CustomCougar> callback)
         {
-            if (!OverrideCalculateTargetPopulation(out int customTarget))
+            mDataManager.ScheduleSpawnModDataProxyRequest(new GetNextAvailableSpawnRequest(mModDataProxy.Guid, mModDataProxy.Scene, false, (availableProxy, result) =>
             {
-                this.LogTraceInstanced($"Spawning target overridden to {customTarget}!");
-                return customTarget;
-            }
-            if (SpawningSuppressedByExperienceMode())
-            {
-                this.LogTraceInstanced($"Spawning suppressed by experience mode");
-                return 0;
-            }
-            if (!mSpawnRegion.m_CanSpawnInBlizzard && GameManager.m_Weather.IsBlizzard())
-            {
-                this.LogTraceInstanced($"Cannot spawn in blizzard");
-                return 0;
-            }
-            return CalculateTargetPopulationInternal();
+                if (result == RequestResult.Succeeded)
+                {
+                    callback(MaybeSpawnCougar(availableProxy, position, rotation));
+                }
+                else
+                {
+                    GenerateNewRandomSpawnModDataProxy((s) =>
+                    {
+                        callback(MaybeSpawnCougar(s, position, rotation));
+                    }, WildlifeMode.Normal, true);
+                }
+            }, false), WildlifeMode.Normal);
         }
 
-
-        private int CalculateTargetPopulationInternal()
+        private CustomCougar MaybeSpawnCougar(SpawnModDataProxy proxy, Vector3 position, Quaternion rotation)
         {
-            int maxSimultaneousSpawns = GameManager.m_TimeOfDay.IsDay()
-                ? GetMaxSimultaneousSpawnsDay()
-                : GetMaxSimultaneousSpawnsNight();
-            maxSimultaneousSpawns -= mSpawnRegion.m_NumTrapped;
-            maxSimultaneousSpawns -= mSpawnRegion.m_NumRespawnsPending;
-            return Math.Max(maxSimultaneousSpawns, 0);
+            proxy.CurrentPosition = position;
+            proxy.CurrentRotation = rotation;
+            CustomBaseAi maybeCustomCougar = InstantiateSpawn(proxy);
+            if (maybeCustomCougar is CustomCougar customCougar)
+            {
+                return customCougar;
+            }
+            else
+            {
+                Main.Logger.Log("Failed to spawn cougar from proxy", ComplexLogger.FlaggedLoggingLevel.Error);
+                return null;
+            }
         }
-        */
     }
 }
