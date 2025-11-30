2663d1d1844019ac31a2726b0fd8ed8981f0a64a
Fuar11 <mcaa6556@gmail.com>
2025-11-30 15:45:15 -0500

    Version++

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -12,7 +12,7 @@ namespace ImprovedCougar
         /// <summary>Who made the mod</summary>
         public const string Author                          = "Fuar";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "2.0.0";
+        public const string Version                         = "2.0.1";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Improved Cougar";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
